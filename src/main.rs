39c407eb00a987b2e964fa8d4b751bb8ef0ea39c
Veror245 <givono123@gmail.com>
2026-07-18 02:34:28 +0530

    validity check almost done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-
+use std::collections::HashMap;
 
 fn main() {
     // let mut board: [[u8; 9]; 9] = [[0; 9]; 9];
@@ -17,7 +17,7 @@ fn main() {
     ];
 
     let sudoku = Sudoku::new(board);
-    dbg!(&sudoku);
+    println!("{}", sudoku.check_valid());
 
     // let m = &sudoku.board.len(); // no of rows
     // let n = &sudoku.board[0].len(); //no of columns in the first row
@@ -30,6 +30,10 @@ fn main() {
         }
         println!();
 
+       }
+
+    for i in (1..9).rev() {
+        println!("{}", 27%i);
     }
     
 }
@@ -52,4 +56,54 @@ impl Sudoku {
 
         (row_size, col_size)
     }
+
+    fn check_valid(&self) -> bool {
+        let (m, n) = self.get_len();
+        let flag = true;
+
+        for i in 0..m{
+            for j in 0..n-1 {
+                if (self.board[i][j] == self.board[i][j+1]) && 
+                (self.board[i][j] != 0) 
+                {return false;}
+            }
+        }
+
+        for i in 0..m{
+            for j in 0..n-1 {
+                if (self.board[j][i] == self.board[j+1][i]) && 
+                (self.board[j][i] != 0) 
+                {return false}
+            }
+        }
+
+        let mut c = 0;
+
+        //TODO: fix index out of bounds
+
+        while c < 27 {
+            let mut freq_map = HashMap::new();
+
+            for i in c..c+3 {
+                for j in c..c+3 {
+                    if self.board[i][j] != 0 {
+                        let count = freq_map.entry(self.board[i][j]).or_insert(0);
+                        *count += 1;
+                    } 
+                }
+            }
+
+            for (_k, v) in freq_map {
+                if v > 1 {return false}
+            }
+
+            c += 3;
+        }
+
+      
+
+
+        flag
+    }
+
 }
