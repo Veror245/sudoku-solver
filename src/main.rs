d6fc0cba4cbc992d0d853cd0973ea722cb4dcb86
Veror245 <givono123@gmail.com>
2026-07-18 00:09:08 +0530

    organised into struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+    // let mut board: [[u8; 9]; 9] = [[0; 9]; 9];
+
+    let mut board = [
+        [0, 0, 0, 0, 7, 0, 0, 0, 6],
+        [4, 0, 0, 2, 0, 0, 8, 0, 0],
+        [0, 9, 2, 8, 0, 0, 0, 0, 0],
+        [9, 0, 5, 0, 0, 4, 0, 0, 0],
+        [0, 4, 0, 0, 3, 0, 0, 6, 0],
+        [0 ,0, 0, 6, 0, 0, 4, 0, 7],
+        [0, 0, 0, 0, 0, 7, 5, 1, 0],
+        [0, 0, 8, 0, 0, 1, 0, 0, 4],
+        [5, 0, 0, 0, 9, 0, 0, 0, 0]
+    ];
+
+    let sudoku = Sudoku::new(board);
+    dbg!(&sudoku);
+
+    // let m = &sudoku.board.len(); // no of rows
+    // let n = &sudoku.board[0].len(); //no of columns in the first row
+
+    let (m, n) = sudoku.get_len();
+
+    for i in 0..m {
+        for j in 0..n {
+            print!("{} ",board[i][j]);
+        }
+        println!();
+
+    }
+    
+}
+
+
+#[derive(Debug)]
+struct Sudoku {
+    board: [[u8; 9]; 9],
+    is_solved: bool
+}
+
+impl Sudoku {
+    fn new(board: [[u8; 9]; 9]) -> Self {
+        Self { board: board, is_solved: false }
+    }
+
+    fn get_len(&self) -> (usize, usize) {
+        let row_size = self.board.len();
+        let col_size = self.board.len();
+
+        (row_size, col_size)
+    }
 }
