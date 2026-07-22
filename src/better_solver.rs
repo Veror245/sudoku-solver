fad62d63ebcb655d86ccbd81b5628a0abbfb3ff7
Veror245 <givono123@gmail.com>
2026-07-23 00:01:07 +0530

    added candidate count strcutre

--- src/better_solver.rs
+++ src/better_solver.rs
@@ -5,7 +5,9 @@ pub struct Solver {
     col_mask: [u16; 9],
     box_mask: [u16; 9],
     
-    neighbors: [[u8; 20]; 81]
+    neighbors: [[u8; 20]; 81], 
+    candidate_bucket: [[usize; 81]; 10],
+    bucket_len: [usize; 10]
 
 }
 
@@ -16,6 +18,8 @@ impl Solver {
         let mut col_mask = [0u16; 9];
         let mut box_mask = [0u16; 9];
         let mut neighbors = [[0u8; 20]; 81];
+        let mut candidate_bucket = [[ 100 as usize; 81]; 10];
+        let mut bucket_len = [0usize; 10];
         
 
         for (idx, cell) in board.iter().enumerate() {
@@ -30,6 +34,7 @@ impl Solver {
                 box_mask[box_idx] |= 1 << *cell;
 
             }
+            
 
             for i in 0..81 {
                 let row_n = i / 9;
@@ -44,8 +49,19 @@ impl Solver {
             }
         }
 
+        for (idx, cell) in board.iter().enumerate() {
 
-        Self { board:board, row_mask:row_mask, col_mask:col_mask, box_mask:box_mask, neighbors: neighbors }
+            if *cell == 0 {
+                let candidates_count = super::better_solver::Solver::get_candidates_count(&Self { board, row_mask, col_mask, box_mask, neighbors, candidate_bucket, bucket_len }, idx);
+                candidate_bucket[candidates_count as usize][bucket_len[candidates_count as usize]]= idx;
+                bucket_len[candidates_count as usize] += 1;
+            }
+
+
+        }
+
+
+        Self { board:board, row_mask:row_mask, col_mask:col_mask, box_mask:box_mask, neighbors: neighbors, candidate_bucket: candidate_bucket, bucket_len:bucket_len }
 
         
     }
@@ -119,5 +135,20 @@ impl Solver {
         (self.box_mask[box_idx] & (1 << candidate) != 0)
     }
 
+    fn get_min_candidate_idx(&self) {
 
+
+    }
+
+    fn bit_mrv(&mut self) -> bool { //placement valid would be candidate_count > 1 else no placement valid
+
+        // if self.get_candidates_count() > 0 {
+
+        // } else {
+        //     return false
+        // }
+
+        false
+
+    }
 }
